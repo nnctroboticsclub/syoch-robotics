b09540a1967de09ba67176f1619c4cd2d15bc5e7
syoch <syoch64@gmail.com>
2024-04-13 05:10:32 +0000

    Update: Added sample code of project generating

--- utils/init_project.sh
+++ utils/init_project.sh
@@ -5,3 +5,6 @@ git init
 git submodule add git@github.com:nnctroboticsclub/syoch-robotics syoch-robotics
 
 cp -v syoch-robotics/skelton/project/* .
+
+# MOUNT=/mnt/st1 PROJECT_DIR=stm32-main SERIAL=066AFF495057717867162927 TAG=s1 LOG_TAG='SerialProxy (UART: 1)' make ns
+# TAG=e PROJECT_DIR=esp32 make ne
