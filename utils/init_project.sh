0db04d2f559002f898aba0db51e7fffc1915e82d
syoch <49235816+syoch@users.noreply.github.com>
2024-04-13 14:20:52 +0900

    Update init_project.sh

--- utils/init_project.sh
+++ utils/init_project.sh
@@ -5,7 +5,7 @@ git init
 git submodule add git@github.com:nnctroboticsclub/syoch-robotics syoch-robotics
 
 cp -v syoch-robotics/skelton/project/* .
-cp -v syoch-robotics/.devcontainer/* .
+cp -v syoch-robotics/.devcontainer .
 
 # MOUNT=/mnt/st1 PROJECT_DIR=stm32-main SERIAL=066AFF495057717867162927 TAG=s1 LOG_TAG='SerialProxy (UART: 1)' make ns
 # TAG=e PROJECT_DIR=esp32 make ne
