4a602a9c5b6d96200ca61d629296973f819f7187
syoch <syoch64@gmail.com>
2024-04-28 02:50:52 +0000

    Update: Added Moduled CAN ackage

--- libs/common/robotics/network/dcan.cpp
+++ libs/common/robotics/network/dcan.cpp
@@ -5,66 +5,18 @@
 using namespace std::chrono_literals;
 
 namespace robotics::network {
-inline void DistributedCAN::HandleMessage(std::uint32_t id,
-                                          std::vector<uint8_t> const &data) {
-  for (auto const &cb : callbacks_) {
-    if (cb.Acceptable(id)) {
-      cb.cb(id, data);
-    }
-  }
-}
-
 DistributedCAN::DistributedCAN(int can_id, std::shared_ptr<CANBase> can)
-    : can_id(can_id), can_(can) {
-  keep_alive_timer.Start();
-}
+    : CANEngine(can_id, can), can_(can) {}
 
 void DistributedCAN::Init() {
-  can_->Init();
-  can_->OnRx([this](std::uint32_t id, std::vector<uint8_t> const &data) {
-    this->HandleMessage(id, data);
-  });
+  CANEngine::Init();
 
-  //* Ping
-  OnMessage(0x7f0, 0x530, [this](std::uint32_t, std::vector<uint8_t>) {
-    can_->Send(0x81 + can_id, {});
-  });
-
-  OnMessage(0x7f0, 0x520, [this](std::uint32_t id, std::vector<uint8_t>) {
-    auto device = id & 0x00f;
-    for (auto &cb : pong_listeners_) {
-      cb(device);
-    }
-  });
-
-  //* Keep alive
-  OnMessage(0x7ff, 0x540, [this](std::uint32_t, std::vector<uint8_t>) {
-    keep_alive_timer.Reset();
-  });
-
-  can_->OnIdle([this]() {
-    auto timer = keep_alive_timer.ElapsedTime();
-    if (keep_alive_available && 300ms < timer) {
-      keep_alive_available = false;
-      for (auto &&cb : this->keep_alive_lost_callbacks_) {
-        cb();
-      }
-    } else if (!keep_alive_available && timer < 300ms) {
-      keep_alive_available = true;
-      for (auto &&cb : this->keep_alive_recovered_callbacks_) {
-        cb();
-      }
-    }
-  });
+  this->RegisterModule(ping_pong_mod);
+  this->RegisterModule(status_mod);
+  this->RegisterModule(keep_alive_mod);
 
   //* Set status to CANReady
-  SetStatus(Statuses::kCANReady);
-}
-
-void DistributedCAN::OnMessage(std::uint32_t mask, std::uint32_t id,
-                               EventCallback::Callback cb) {
-  EventCallback event_cb = {mask, id, cb};
-  callbacks_.emplace_back(event_cb);
+  SetStatus(can_module::Status::Statuses::kCANReady);
 }
 
 CANBase::Capability DistributedCAN::GetCapability() {
@@ -75,33 +27,24 @@ float DistributedCAN::GetBusLoad() { return can_->GetBusLoad(); }
 
 void DistributedCAN::OnRx(CANBase::RxCallback cb) { can_->OnRx(cb); }
 void DistributedCAN::OnTx(CANBase::TxCallback cb) { can_->OnTx(cb); }
-void DistributedCAN::OnIdle(CANBase::IdleCallback cb) { can_->OnIdle(cb); }
 
 void DistributedCAN::SendKeepAlive() { can_->Send(0xfc, {}); }
-void DistributedCAN::OnKeepAliveLost(KeepAliveLostCallback cb) {
-  this->keep_alive_lost_callbacks_.emplace_back(cb);
+void DistributedCAN::OnKeepAliveLost(can_module::KeepAlive::KeepAliveLostCallback cb) {
+  keep_alive_mod.OnKeepAliveLost(cb);
 }
-void DistributedCAN::OnKeepAliveRecovered(KeepAliveRecoverdCallback cb) {
-  this->keep_alive_recovered_callbacks_.emplace_back(cb);
-}
-
-int DistributedCAN::Send(uint8_t element_id, std::vector<uint8_t> const &data) {
-  return can_->Send(element_id, data);
+void DistributedCAN::OnKeepAliveRecovered(can_module::KeepAlive::KeepAliveRecoverdCallback cb) {
+  keep_alive_mod.OnKeepAliveRecovered(cb);
 }
 
-void DistributedCAN::SetStatus(Statuses status) {
-  std::vector<uint8_t> payload(4);
-  payload[0] = 0xf0;
-  payload[1] = (can_id >> 0x08) && 0xff;
-  payload[2] = (can_id >> 0x00) & 0xff;
-  payload[3] = static_cast<uint8_t>(status);
-
-  can_->Send(0xa0, payload);
+void DistributedCAN::SetStatus(can_module::Status::Statuses status) {
+  status_mod.SetStatus(status);
 }
 
-void DistributedCAN::Ping() { can_->Send(0x80, {}); }
-void DistributedCAN::OnPong(PongListener cb) {
-  pong_listeners_.emplace_back(cb);
+void DistributedCAN::Ping() {
+  ping_pong_mod.Ping();
+ }
+void DistributedCAN::OnPong(can_module::PingPong::PongListener cb) {
+  ping_pong_mod.OnPong(cb);
 }
 
 }  // namespace robotics::network
--- libs/common/robotics/network/dcan.hpp
+++ libs/common/robotics/network/dcan.hpp
@@ -6,80 +6,42 @@
 #include <iomanip>
 #include <memory>
 
-#include "can_base.hpp"
+#include "can_module.hpp"
 
 #include "../platform/timer.hpp"
 
 namespace robotics::network {
-class DistributedCAN {
- public:
-  enum class Statuses : uint8_t {
-    kReady = 0x00,
-    kCANReady = 0x01,
-    kInitializingESC0 = 0x02,
-    kInitializingESC1 = 0x03,
-    kInitializingGyro = 0x04,
-  };
-
-  struct EventCallback {
-    using Callback =
-        std::function<void(std::uint32_t id, std::vector<uint8_t> const &data)>;
-
-    // accepted when can.id & mask == id
-    const std::uint32_t mask;
-    const std::uint32_t id;
-    Callback cb;
-
-    inline bool Acceptable(std::uint32_t cid) const {
-      return (cid & mask) == id;
-    }
-  };
-
-  using KeepAliveLostCallback = std::function<void()>;
-  using KeepAliveRecoverdCallback = std::function<void()>;
-
-  using PongListener = std::function<void(uint8_t device)>;
-
+class DistributedCAN : public CANEngine {
  private:
-  int can_id = 0x7;
   std::shared_ptr<CANBase> can_;
-  int last_keep_alive = 0;
-  robotics::system::Timer keep_alive_timer;
 
-  std::vector<EventCallback> callbacks_;
-
-  bool keep_alive_available;
-  std::vector<KeepAliveLostCallback> keep_alive_lost_callbacks_;
-  std::vector<KeepAliveRecoverdCallback> keep_alive_recovered_callbacks_;
-
-  std::vector<PongListener> pong_listeners_;
-
-  inline void HandleMessage(std::uint32_t id, std::vector<uint8_t> const &data);
+  can_module::Status status_mod{};
+  can_module::KeepAlive keep_alive_mod{};
+  can_module::PingPong ping_pong_mod{};
 
  public:
   DistributedCAN(int can_id, std::shared_ptr<CANBase> can);
+  ~DistributedCAN() = default;
   void Init();
 
-  void OnMessage(std::uint32_t mask, std::uint32_t id,
-                 EventCallback::Callback cb);
-
+  // CANBase exports
   CANBase::Capability GetCapability();
   float GetBusLoad();
 
   void OnRx(CANBase::RxCallback cb);
   void OnTx(CANBase::TxCallback cb);
-  void OnIdle(CANBase::IdleCallback cb);
 
+  // KeepAlive exports
   void SendKeepAlive();
-  void OnKeepAliveLost(KeepAliveLostCallback cb);
-  void OnKeepAliveRecovered(KeepAliveRecoverdCallback cb);
+  void OnKeepAliveLost(can_module::KeepAlive::KeepAliveLostCallback cb);
+  void OnKeepAliveRecovered(
+      can_module::KeepAlive::KeepAliveRecoverdCallback cb);
 
-  int Send(uint8_t element_id, std::vector<uint8_t> const &data);
-  void SetStatus(Statuses status);
+  // Status exports
+  void SetStatus(can_module::Status::Statuses status);
 
+  // PingPong exports
   void Ping();
-  void OnPong(PongListener cb);
-
-  int GetCanId() { return can_id; }
+  void OnPong(can_module::PingPong::PongListener cb);
 };
 }  // namespace robotics::network
