c8e8e4d67f99635099715357f99e94d6201e0664
syoch <syoch64@gmail.com>
2024-05-12 08:25:36 +0000

    Update: fix

--- libs/common/robotics/node/node_inspector.cpp
+++ libs/common/robotics/node/node_inspector.cpp
@@ -14,71 +14,29 @@ namespace {
 class NodeInfo {
   static uint16_t latest_id_;
 
+  std::array<uint8_t, 4> data_payload;
+
  public:
   uint16_t type;
   uint16_t id;
-
   std::vector<std::shared_ptr<NodeInfo>> links;
 
   NodeInfo(uint16_t type) : type(type), id(latest_id_++) {}
+
+  void SendData();
+  void SendLink(int index);
+  void SendLinks();
+
+  void SetData(std::array<uint8_t, 4> data) { data_payload = data; }
 };
 
 uint16_t NodeInfo::latest_id_ = 0;
-}  // namespace
 
-class NodeInspector::Impl {
-  NodeInfo node_info;
+class GlobalNodeInspector {
+  std::shared_ptr<network::DistributedCAN> can_;
+  uint32_t message_id;
 
-  std::array<uint8_t, 4> data_payload;
-
-  void Send(uint16_t type, std::array<uint8_t, 4> data) {
-    if (can_.can_ == nullptr) {
-      return;
-    }
-
-    std::vector<uint8_t> payload{
-        (unsigned char)(type >> 8),
-        (unsigned char)(type & 0xFF),
-        (unsigned char)(node_info.id >> 8),
-        (unsigned char)(node_info.id & 0xFF),
-        data[0],
-        data[1],
-        data[2],
-        data[3],
-    };
-
-    can_.can_->Send(can_.message_id, payload);
-  }
-
-  void SendLink(int index) {
-    if (index < 0 || index >= node_info.links.size()) {
-      return;
-    }
-
-    Send(0xFFFF, {(unsigned char)(node_info.links[index]->type >> 8),
-                  (unsigned char)(node_info.links[index]->type & 0xFF),
-                  (unsigned char)(node_info.links[index]->id >> 8),
-                  (unsigned char)(node_info.links[index]->id & 0xFF)});
-  }
-
-  void SendData() { Send(node_info.type, data_payload); }
-
-  void SendLinks() {
-    if (can_.can_ == nullptr) {
-      return;
-    }
-
-    for (int i = 0; i < node_info.links.size(); i++) {
-      SendLink(i);
-    }
-  }
-
-  // CAN Integration
-  static inline struct CAN {
-    std::shared_ptr<network::DistributedCAN> can_;
-    uint32_t message_id;
-  } can_;
-  static inline std::vector<std::shared_ptr<NodeInspector::Impl>> nodes_;
+  static inline std::vector<std::shared_ptr<NodeInfo>> nodes_;
 
   static void HandleCanMessage(std::uint32_t id, std::vector<uint8_t> payload) {
     enum class Command : uint8_t {
@@ -87,7 +45,7 @@ class NodeInspector::Impl {
       kRequestNodeLinks,
     };
 
-    Command command = (Command)payload[0];
+    auto command = (Command)payload[0];
 
     switch (command) {
       case Command::kShowAll: {
@@ -102,7 +60,7 @@ class NodeInspector::Impl {
         uint16_t id = (payload[1] << 8) | payload[2];
 
         for (auto node : nodes_) {
-          if (node->node_info.id == id) {
+          if (node->id == id) {
             node->SendData();
             break;
           }
@@ -114,7 +72,7 @@ class NodeInspector::Impl {
         uint16_t id = (payload[1] << 8) | payload[2];
 
         for (auto node : nodes_) {
-          if (node->node_info.id == id) {
+          if (node->id == id) {
             node->SendLinks();
             break;
           }
@@ -130,13 +88,70 @@ class NodeInspector::Impl {
   }
 
  public:
+  void Send(uint16_t type, uint16_t id, std::array<uint8_t, 4> data) {
+    if (can_ == nullptr) {
+      return;
+    }
+
+    std::vector<uint8_t> payload{
+        (unsigned char)(type >> 8),
+        (unsigned char)(type & 0xFF),
+        (unsigned char)(id >> 8),
+        (unsigned char)(id & 0xFF),
+        data[0],
+        data[1],
+        data[2],
+        data[3],
+    };
+
+    can_->Send(message_id, payload);
+  }
+
+  void AddNode(std::shared_ptr<NodeInfo> node) { nodes_.push_back(node); }
+
+  void RegisterCAN(std::shared_ptr<network::DistributedCAN> can) {
+    can_ = can;
+    message_id = 0x4d0 + can->GetDeviceId();
+
+    can_->OnMessage(0x7f8, 0x4d8, HandleCanMessage);
+  }
+};
+
+GlobalNodeInspector g_inspector;
+
+void NodeInfo::SendData() { g_inspector.Send(type, id, data_payload); }
+
+void NodeInfo::SendLink(int index) {
+  if (0 < index || index >= links.size()) {
+    return;
+  }
+
+  auto link = links[index];
+
+  g_inspector.Send(
+      0xFFFF, id,
+      {(unsigned char)(link->type >> 8), (unsigned char)(link->type & 0xFF),
+       (unsigned char)(link->id >> 8), (unsigned char)(link->id & 0xFF)});
+}
+
+void NodeInfo::SendLinks() {
+  for (int i = 0; i < links.size(); i++) {
+    SendLink(i);
+  }
+}
+}  // namespace
+
+class NodeInspector::Impl {
+  NodeInfo node_info;
+
+ public:
   Impl(uint16_t type) : node_info(type) {
-    nodes_.push_back((std::shared_ptr<NodeInspector::Impl>)this);
+    g_inspector.AddNode(std::shared_ptr<NodeInfo>(&node_info));
   }
 
   void Update(std::array<uint8_t, 4> data) {
-    data_payload = data;
-    SendData();
+    node_info.SetData(data);
+    node_info.SendData();
   }
 
   void Link(std::shared_ptr<Impl> other) {
@@ -144,14 +159,11 @@ class NodeInspector::Impl {
 
     node_info.links.push_back(info_p);
 
-    SendLink(node_info.links.size() - 1);
+    node_info.SendLink(node_info.links.size() - 1);
   }
 
   static void RegisterCAN(std::shared_ptr<network::DistributedCAN> can) {
-    can_.can_ = can;
-    can_.message_id = 0x4d0 + can->GetDeviceId();
-
-    can_.can_->OnMessage(0x7f8, 0x4d8, HandleCanMessage);
+    g_inspector.RegisterCAN(can);
   }
 };
 
@@ -160,7 +172,7 @@ NodeInspector::NodeInspector(uint16_t type)
 
 NodeInspector::~NodeInspector() = default;
 
-void NodeInspector::Link(NodeInspector &other_inspector) {
+void NodeInspector::Link(NodeInspector& other_inspector) {
   impl_->Link(other_inspector.impl_);
 }
 
