f607d94e499cb69cb7c6a7c55188a05a528f0665
syoch <syoch64@gmail.com>
2024-06-21 16:14:32 +0000

    Update(test): some

--- utils/test.py
+++ utils/test.py
@@ -22,8 +22,34 @@ def encode_rep(data: bytes) -> bytes:
 
 def encode_ssp(svc: int, data: bytes) -> bytes:
     ret = svc.to_bytes(2, "big")
-    ret += b"\x00\x00"
     ret += data
+    return ret
+
+
+def encode_froute(dest: int, data: bytes) -> bytes:
+    ret = b""
+    ret += b"\x80"  # life
+    ret += b"\x80"  # from
+    ret += dest.to_bytes(1, "big")
+    ret += b"\x00"  # Flags
+    ret += data
+    return ret
+
+
+def encode_froute_adv(addr_self: int) -> bytes:
+    ret = b""
+    ret += b"\x80"  # life
+    ret += addr_self.to_bytes(1, "big")
+    ret += b"\0"  # goal
+    ret += b"\x02"  # Flags
+
+    return ret
+
+
+def encode(dest: int, svc_id: int, key_id: int, value: bytes) -> bytes:
+    ret = encode_ssp(svc_id, key_id.to_bytes(2, "big") + value)
+    ret = encode_froute(dest, ret)
+    ret = encode_rep(ret)
     return ret
 
 
@@ -50,46 +76,55 @@ def generate_packet_vs_test1():
 
 
 def generate_packet_ni_init():
-    payload = b"\x01"
+    payload = b"\x00"
     return encode_rep(encode_ssp(0x0400, payload))
 
 
 async def main():
     fep = await FepClient.connect("localhost", 31337)
 
+    packets_received = 0
+
     @fep.on_packet
     def on_packet(addr, packet):
+        nonlocal packets_received
         rep_data = packet[4:-2]
 
-        svc_id = int.from_bytes(rep_data[:2], "big")
-        svc_payload = rep_data[4:]
+        frt_header = rep_data[:4]
+        frt_data = rep_data[4:]
+
+        svc_id = int.from_bytes(frt_data[:2], "big")
+        svc_payload = frt_data[2:]
 
         key_id = int.from_bytes(svc_payload[:2], "big")
         value = svc_payload[2:]
 
         print(f"Received packet: {addr} {svc_id}-{key_id} {value}")
 
-    print("\x1b[1;32m[*]\x1b[m Sending the test command")
+        packets_received += 1
 
-    """ await fep.transmit(DEV1_ADDR, encode_rep(encode_ssp(0x0000, b"\x00\x00")))
-    await asyncio.sleep(0.5)
-    await fep.transmit(DEV1_ADDR, encode_rep(encode_ssp(0x0000, b"\x00\x01")))
-    await asyncio.sleep(0.5)
-    await fep.transmit(DEV1_ADDR, encode_rep(encode_ssp(0x0000, b"\x00\x02")))
+    print("\x1b[1;32m[*]\x1b[m Sending the bad command")
+    await fep.transmit(
+        DEV2_ADDR, encode_rep(encode_froute(0x02, encode_ssp(0x0000, b"\x00\x05")))
+    )
+
+    print("\x1b[1;32m[*]\x1b[m Advertising self")
+    await fep.transmit(DEV1_ADDR, encode_rep(encode_froute_adv(0x80)))
+    await fep.transmit(DEV2_ADDR, encode_rep(encode_froute_adv(0x80)))
     await asyncio.sleep(0.5)
 
-    await fep.transmit(DEV2_ADDR, encode_rep(encode_ssp(0x0000, b"\x00\x00")))
-    await asyncio.sleep(0.5)
-    await fep.transmit(DEV2_ADDR, encode_rep(encode_ssp(0x0000, b"\x00\x01")))
-    await asyncio.sleep(0.5)
-    await fep.transmit(DEV2_ADDR, encode_rep(encode_ssp(0x0000, b"\x00\x02")))
-    await asyncio.sleep(0.5) """
+    print("----- Sending 20 packets with 50ms delay...")
+    for _ in range(20):
+        await fep.transmit(DEV2_ADDR, encode(0x02, 0x0000, 2, b""))
+        await asyncio.sleep(0.05)
+    print("----- Result")
 
-    await fep.transmit(DEV1_ADDR, generate_packet_ni_init())
-    await fep.transmit(DEV1_ADDR, generate_packet_vs_test1())
-    await asyncio.sleep(1)
-    await fep.transmit(DEV1_ADDR, generate_packet_vs_test1())
-    await asyncio.sleep(5)
+    score = packets_received
+    print(f"Score: {score}")
+
+    print(f"{packets_received/20*100}% of packets received in 2secs")
+    speed = packets_received / (20 * 0.5)
+    print(f"Speed: {speed} packets/sec")
 
     print("\x1b[1;32m[*]\x1b[m Done")
 
