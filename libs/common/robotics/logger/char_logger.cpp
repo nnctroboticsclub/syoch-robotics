37c4073a6b59411c398258a45bc5f77c8d30d637
syoch <syoch64@gmail.com>
2024-06-28 07:08:11 +0000

    Update(CharLogger): Added User flushing mode

--- libs/common/robotics/logger/char_logger.cpp
+++ libs/common/robotics/logger/char_logger.cpp
@@ -5,31 +5,52 @@
 namespace robotics::logger {
 CharLogger::CharLogger(const char* id, const char* tag) : logger(id, tag) {}
 
+void CharLogger::Cache(char c) {
+  if (isprint(c)) {
+    if (strlen(data) + 1 > sizeof(data)) return;
+    data[strlen(data)] = c;
+  } else {
+    if (strlen(data) + 4 > sizeof(data)) return;
+    data[strlen(data)] = '\\';
+    data[strlen(data)] = 'x';
+    data[strlen(data)] = "0123456789ABCDEF"[c >> 4];
+    data[strlen(data)] = "0123456789ABCDEF"[c & 0xF];
+  }
+}
+
 void CharLogger::SetLevel(core::Level level) { this->level = level; }
 
 void CharLogger::SetCachingMode(CachingMode mode) { caching_mode = mode; }
 
+void CharLogger::SetHeader(char* header) {
+  strncpy(this->header, header, sizeof(this->header));
+}
+
 void CharLogger::Log(char c) {
   if (caching_mode == CachingMode::kNone) {
-    logger.Log(level, "%c", c);
-  }
-
-  if (caching_mode == CachingMode::kNewLine) {
+    logger.Log(level, "%s%c", header, c);
+  } else if (caching_mode == CachingMode::kNewLine) {
     if (c == '\n' || strlen(data) >= sizeof(data) - 1) {
-      logger.Log(level, "%s", data);
-      memset(data, 0, sizeof(data));
+      Flush();
+      ClearBuffer();
       return;
     } else if (c == '\r') {
       return;
-    } else if (isprint(c)) {
-      data[strlen(data)] = c;
     } else {
-      data[strlen(data)] = '\\';
-      data[strlen(data)] = 'x';
-      data[strlen(data)] = "0123456789ABCDEF"[c >> 4];
-      data[strlen(data)] = "0123456789ABCDEF"[c & 0xF];
+      Cache(c);
     }
+  } else if (caching_mode == CachingMode::kUser) {
+    Cache(c);
   }
 }
+
+void CharLogger::LogN(char* c, size_t n) {
+  for (size_t i = 0; i < n; i++) {
+    Log(c[i]);
+  }
+}
+
+void CharLogger::Flush() { logger.Log(level, "%s%s", header, data); }
+void CharLogger::ClearBuffer() { memset(data, 0, sizeof(data)); }
 
 }  // namespace robotics::logger
--- libs/common/robotics/logger/char_logger.hpp
+++ libs/common/robotics/logger/char_logger.hpp
@@ -8,21 +8,30 @@ namespace robotics::logger {
 struct CharLogger {
   GenericLogger logger;
   char data[512] = {};
+  char header[32] = {0};
 
   core::Level level = core::Level::kInfo;
 
   enum class CachingMode {
     kNone,
     kNewLine,
+    kUser
   } caching_mode = CachingMode::kNewLine;
+
+  void Cache(char c);
 
  public:
   CharLogger(const char* id, const char* tag);
 
   void SetLevel(core::Level level);
-
   void SetCachingMode(CachingMode mode);
+  void SetHeader(char* header);
 
   void Log(char c);
+
+  void LogN(char* c, size_t n);
+
+  void Flush();
+  void ClearBuffer();
 };
 }  // namespace robotics::logger
