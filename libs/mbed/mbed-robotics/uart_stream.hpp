b89afc84fb6dc02b713331f803c5b5821f465945
syoch <syoch64@gmail.com>
2024-06-29 14:06:09 +0000

    Update(Mbed, UARTStream): Support Rebaud

--- libs/mbed/mbed-robotics/uart_stream.hpp
+++ libs/mbed/mbed-robotics/uart_stream.hpp
@@ -8,21 +8,46 @@
 
 namespace robotics::network {
 class UARTStream : public Stream<uint8_t> {
-  mbed::UnbufferedSerial upper_stream;
+  logger::Logger logger{"uart.nw", "\x1b[1;35m   UART  \x1b[m"};
+  mbed::UnbufferedSerial* upper_stream = nullptr;
 
- public:
-  void Send(uint8_t* data, uint32_t len) override {
-    upper_stream.write(data, len);
-  }
+  PinName tx, rx;
 
-  UARTStream(PinName tx, PinName rx, int baud) : upper_stream(tx, rx, baud) {
-    upper_stream.attach([this]() {
-      while (upper_stream.readable()) {
+  void Init(int baudrate) {
+    if (upper_stream) return;
+
+    upper_stream = new mbed::UnbufferedSerial(tx, rx, baudrate);
+    upper_stream->enable_input(true);
+    upper_stream->enable_output(true);
+    upper_stream->attach([this]() {
+      while (upper_stream->readable()) {
         uint8_t data[128];
-        uint32_t length = upper_stream.read(&data, 1024);
+        uint32_t length = upper_stream->read(&data, 1024);
         DispatchOnReceive(data, length);
       }
     });
+  }
+
+  void Deinit() {
+    if (!upper_stream) return;
+    upper_stream->enable_input(false);
+    upper_stream->enable_output(false);
+    upper_stream->close();
+    delete upper_stream;
+    upper_stream = nullptr;
+  }
+
+ public:
+  UARTStream(PinName tx, PinName rx, int baud) : tx(tx), rx(rx) { Init(baud); }
+
+  void Send(uint8_t* data, uint32_t len) override {
+    if (!upper_stream) return;
+    upper_stream->write(data, len);
+  }
+
+  void Rebaud(int baud) {
+    Deinit();
+    Init(baud);
   }
 };
 }  // namespace robotics::network
