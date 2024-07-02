e7f1f477893bd9ca0adc0f84c6fba66a244a8e90
timniku <myonniku@gmail.com>
2024-07-02 02:09:35 +0000

    Fixed(IkakoRobomasNode): Tested IkakoRobomasNode

--- libs/mbed/mbed-robotics/ikakorobomas_node.hpp
+++ libs/mbed/mbed-robotics/ikakorobomas_node.hpp
@@ -3,47 +3,58 @@
 #include <ikarashiCAN_mk2.h>
 #include <mbed.h>
 
-#include <vector>
-#include <vector>
-#include <vector>
-#include <vector>
 #include <robotics/node/node.hpp>
+#include <vector>
+
+struct controller_param {
+  float Ts = 0.001;
+  float kp = 1.0;
+  float ki = 0;
+  float kd = 0;
+  float current_limit = 5.0;
+  float omega = 1 * 2 * M_PI;
+};
 
 class IkakoRobomasNode {
-  struct controller_param
-  {
-    float Ts = 0.001;
-    float kp = 1.0;
-    float ki = 0;
-    float kd = 0;
-    float current_limit = 5.0;
-    float omega = 1 * 2 * M_PI;
-  };
   static controller_param cprm;
   static MotorParams *m3508;
 
  public:
   robotics::Node<float> velocity;
-  MotorController controller{ControlType::VELOCITY, m3508, cprm.Ts, cprm.omega};
+  MotorController *controller;
+  IkakoM3508 &super;
 
  public:
   IkakoRobomasNode(
-    IkakoM3508 &super //ikakoMDC *mdc_ = nullptr;
-    //https://github.com/nnctroboticsclub/IkakoRobomasのm3という配列を指すポインタである
-    //m3{aa,ggg}でいうaa,gggとかのことm3はsuperとして扱っていい
-  ) {
+      IkakoM3508 &super  // ikakoMDC *mdc_ = nullptr;
+      // https://github.com/nnctroboticsclub/IkakoRobomasのm3という配列を指すポインタである
+      )
+      : super(super) {
     m3508 = super.get_motor_params();
     m3508->D = 0.0;
     m3508->J = 0.04;
 
-    controller.set_limit(-cprm.current_limit, cprm.current_limit);
-    controller.set_pid_gain(cprm.kp, cprm.ki, cprm.kd);
-    controller.start();
-    
-    velocity.SetChangeCallback([this, super](float velo){//veloとかは引数の名前で勝手に定義してよい
-      //処理をかけ
-      controller.set_reference(velo);
-      super.set_ref(controller.get_output());
-  });
+    controller =
+        new MotorController(ControlType::VELOCITY, m3508, cprm.Ts, cprm.omega);
+    controller->set_limit(-cprm.current_limit, cprm.current_limit);
+    controller->set_pid_gain(cprm.kp, cprm.ki, cprm.kd);
+    controller->start();
+
+    velocity.SetChangeCallback(
+        [this](float velo) {  // veloとかは引数の名前で勝手に定義してよい
+          // 処理をかけ
+          controller->set_reference(velo);
+        });
+  }
+  bool GetReadFlag() { return super.get_read_flag(); }
+
+  void Update() {
+    if (super.get_read_flag()) controller->set_response(super.get_vel());
+    controller->update();
+
+    super.set_ref(controller->get_output());
   }
 };
+
+controller_param IkakoRobomasNode::cprm;
+MotorParams *IkakoRobomasNode::m3508;
