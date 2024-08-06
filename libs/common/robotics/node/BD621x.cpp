ed9d31df2e2ae881d32cdceaee428822934e9835
syoch <syoch64@gmail.com>
2024-08-06 14:16:52 +0000

    Fix: Fixed BD621 period

--- libs/common/robotics/node/BD621x.cpp
+++ libs/common/robotics/node/BD621x.cpp
@@ -9,24 +9,24 @@ namespace robotics::node {
 BD621x::BD621x(std::shared_ptr<robotics::driver::PWMBase> rin,
                std::shared_ptr<robotics::driver::PWMBase> fin)
     : rin_(rin), fin_(fin) {
-  fin_->period_us(200);
-  rin_->period_us(200);
-  fin_->pulsewidth_us(200);
-  rin_->pulsewidth_us(200);
+  fin_->period_us(50);
+  rin_->period_us(50);
+  fin_->pulsewidth_us(50);
+  rin_->pulsewidth_us(50);
 }
 
 void BD621x::SetSpeed(float speed) {
   if (0.05 < speed && speed < 0.05) speed = 0;
 
   if (speed == 0) {
-    fin_->pulsewidth_us(1*200.0);
-    rin_->pulsewidth_us(1*200.0);
+    fin_->pulsewidth_us(1 * 50.0);
+    rin_->pulsewidth_us(1 * 50.0);
   } else if (speed > 0) {
-    fin_->pulsewidth_us(speed*200.0);
-    rin_->pulsewidth_us(0*200.0);
+    fin_->pulsewidth_us(speed * 50.0);
+    rin_->pulsewidth_us(0 * 50.0);
   } else if (speed < 0) {
-    fin_->pulsewidth_us(0*200.0);
-    rin_->pulsewidth_us(-speed*200.0);
+    fin_->pulsewidth_us(0 * 50.0);
+    rin_->pulsewidth_us(-speed * 50.0);
   }
 }
 
