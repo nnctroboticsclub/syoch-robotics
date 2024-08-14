0143973d863dacdd760a9a655eb968782dd7ceeb
syoch <syoch64@gmail.com>
2024-08-14 06:52:57 +0000

    Update: Configured BD621x timing

--- libs/common/robotics/node/BD621x.cpp
+++ libs/common/robotics/node/BD621x.cpp
@@ -9,24 +9,28 @@ namespace robotics::node {
 BD621x::BD621x(std::shared_ptr<robotics::driver::PWMBase> rin,
                std::shared_ptr<robotics::driver::PWMBase> fin)
     : rin_(rin), fin_(fin) {
-  fin_->period_us(50);
-  rin_->period_us(50);
-  fin_->pulsewidth_us(50);
-  rin_->pulsewidth_us(50);
+  fin_->period_us(10);
+  rin_->period_us(10);
+  fin_->pulsewidth_us(10);
+  rin_->pulsewidth_us(10);
 }
 
 void BD621x::SetSpeed(float speed) {
   if (0.05 < speed && speed < 0.05) speed = 0;
 
   if (speed == 0) {
-    fin_->pulsewidth_us(1 * 50.0);
-    rin_->pulsewidth_us(1 * 50.0);
+    fin_->pulsewidth_us(1 * 10.0);
+    rin_->pulsewidth_us(1 * 10.0);
   } else if (speed > 0) {
-    fin_->pulsewidth_us(speed * 50.0);
-    rin_->pulsewidth_us(0 * 50.0);
+    // fin_->pulsewidth_us(10);
+    // rin_->pulsewidth_us(0);
+    fin_->pulsewidth_us(speed * 10.0);
+    rin_->pulsewidth_us(0 * 10.0);
   } else if (speed < 0) {
-    fin_->pulsewidth_us(0 * 50.0);
-    rin_->pulsewidth_us(-speed * 50.0);
+    // fin_->pulsewidth_us(0);
+    // rin_->pulsewidth_us(10);
+    fin_->pulsewidth_us(0 * 10.0);
+    rin_->pulsewidth_us(-speed * 10.0);
   }
 }
 
