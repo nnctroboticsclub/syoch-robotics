1050a75cf5bec636817357d68cf38f83a87abf70
syoch <syoch64@gmail.com>
2024-09-11 14:02:05 +0000

    Update: Added Send loop protect

--- libs/common/robotics/network/ssp/value_store.hpp
+++ libs/common/robotics/network/ssp/value_store.hpp
@@ -21,7 +21,12 @@ class ValueStoreService : public robotics::network::ssp::SSP_Service<Context> {
     };
   };
 
-  std::unordered_map<uint32_t, robotics::node::GenericNode*> nodes_;
+  struct RegisteredNode {
+    robotics::node::GenericNode* node;
+    bool receiving;
+  };
+
+  std::unordered_map<uint32_t, RegisteredNode> nodes_;
 
   uint8_t tx_buffer[16];
 
@@ -35,20 +40,31 @@ class ValueStoreService : public robotics::network::ssp::SSP_Service<Context> {
       }
 
       RxItem rx;
-      std::memcpy(&rx.raw_data, data, len);
+      rx.id = (data[0] << 24) | (data[1] << 16) | (data[2] << 8) | data[3];
+      std::memcpy(&rx.data[0], &data[4], 4);
 
       if (nodes_.find(rx.id) == nodes_.end()) {
+        this->logger.Error("[RX] Node not found (node = %08x)", rx.id);
         return;
       }
 
-      auto node = nodes_[rx.id];
-      node->LoadFromBytes({rx.data[0], rx.data[1], rx.data[2], rx.data[3]});
+      auto& node = nodes_[rx.id];
+
+      node.receiving = true;
+      node.node->LoadFromBytes(
+          {rx.data[0], rx.data[1], rx.data[2], rx.data[3]});
+      node.receiving = false;
     });
   }
 
   void AddController(uint32_t id, Context remote,
                      robotics::node::GenericNode& node) {
     node.OnChanged([this, id, remote, &node]() {
+      if (nodes_.find(id) == nodes_.end()) {
+        this->logger.Verbose("[Tx] Node not found (0x%08x)", id);
+        return;
+      }
+
       auto data = node.Encode();
       tx_buffer[0] = (id >> 24) & 0xff;
       tx_buffer[1] = (id >> 16) & 0xff;
@@ -56,9 +72,13 @@ class ValueStoreService : public robotics::network::ssp::SSP_Service<Context> {
       tx_buffer[3] = id & 0xff;
       std::memcpy(&tx_buffer[4], &data.front(), 4);
 
+      if (nodes_[id].receiving) {
+        return;
+      }
+
       this->Send(remote, tx_buffer, 8);
     });
-    nodes_[id] = &node;
+    nodes_[id] = RegisteredNode{&node, false};
   }
 };
 }  // namespace robotics::network::ssp
