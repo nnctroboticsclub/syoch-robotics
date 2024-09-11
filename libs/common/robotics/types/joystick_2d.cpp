1fe789b0ee95bb0c6f58b341b75dcb218f5a0463
timniku <myonniku@gmail.com>
2024-09-11 07:43:08 +0000

    Update: Moved Joystick2D Encoder/Decoder

---
+++ libs/common/robotics/types/joystick_2d.cpp
@@ -0,0 +1,27 @@
+#include <robotics/node/node.hpp>
+#include <robotics/types/joystick_2d.hpp>
+
+namespace robotics::node {
+    template <>
+std::array<uint8_t, 4> NodeEncoder<JoyStick2D>::Encode(JoyStick2D value) {
+  uint16_t x = value[0] * 0x7FFF + 0x7FFF;
+  uint16_t y = value[1] * 0x7FFF + 0x7FFF;
+
+  std::array<uint8_t, 4> data_array;
+  data_array[0] = x >> 8;
+  data_array[1] = x & 0xFF;
+  data_array[2] = y >> 8;
+  data_array[3] = y & 0xFF;
+
+  return data_array;
+}
+
+template <>
+JoyStick2D NodeEncoder<JoyStick2D>::Decode(std::array<uint8_t, 4> data) {
+  uint16_t x = (data[0] << 8) | data[1];
+  uint16_t y = (data[2] << 8) | data[3];
+
+  return {static_cast<float>(x - 0x7FFF) / 0x7FFF,
+          static_cast<float>(y - 0x7FFF) / 0x7FFF};
+}
+}
--- libs/common/robotics/types/joystick_2d.hpp
+++ libs/common/robotics/types/joystick_2d.hpp
@@ -9,29 +9,3 @@ using JoyStick2D = Vector<float, 2>;
 
 }  // namespace types
 }  // namespace robotics
-
-namespace robotics::node {
-template <>
-std::array<uint8_t, 4> NodeEncoder<JoyStick2D>::Encode(JoyStick2D value) {
-  uint16_t x = value[0] * 0x7FFF + 0x7FFF;
-  uint16_t y = value[1] * 0x7FFF + 0x7FFF;
-
-  std::array<uint8_t, 4> data_array;
-  data_array[0] = x >> 8;
-  data_array[1] = x & 0xFF;
-  data_array[2] = y >> 8;
-  data_array[3] = y & 0xFF;
-
-  return data_array;
-}
-
-template <>
-JoyStick2D NodeEncoder<JoyStick2D>::Decode(std::array<uint8_t, 4> data) {
-  uint16_t x = (data[0] << 8) | data[1];
-  uint16_t y = (data[2] << 8) | data[3];
-
-  return {static_cast<float>(x - 0x7FFF) / 0x7FFF,
-          static_cast<float>(y - 0x7FFF) / 0x7FFF};
-}
-
-}  // namespace robotics::node
