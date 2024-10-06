888811fd3673c837df001c0aa6b8bd40ec02e369
syoch <syoch64@gmail.com>
2024-10-06 10:55:53 +0000

    Update: Removed libs/cmake

--- libs/cmake/MbedCE-Toolchain.cmake
+++
@@ -1,3 +0,0 @@
-find_package(mbed-ce REQUIRED)
-
-include(${mbed-ce_SOURCE_DIR}/tools/cmake/app.cmake)
--- libs/cmake/MbedCE.cmake
+++
@@ -1,4 +0,0 @@
-include(MbedCE-Toolchain)
-
-find_package(mbed-ce REQUIRED)
-add_subdirectory(${mbed-ce_SOURCE_DIR} mbed-ce)
