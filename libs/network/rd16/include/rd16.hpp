4b0d72fe05c9cb3c1e6e6a95b88c52f692069caf
syoch <syoch64@gmail.com>
2024-10-13 13:41:05 +0000

    Update: Added RD16

---
+++ libs/network/rd16/include/rd16.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdint>
+
+namespace robotics::network {
+class RD16 {
+  uint16_t current = 0;
+
+ public:
+  void Reset() { current = 0; }
+
+  void operator<<(uint16_t x) {
+    current = ((current & 0x007F) << 9) | ((current & 0xff80) >> 7);
+    current ^= 0x35ca;
+    current ^= x;
+  }
+
+  void operator<<(uint8_t x) { *this << ((uint16_t)((x << 8) | x)); }
+
+  uint16_t Get() const { return current; }
+};
+
+}  // namespace robotics::network
