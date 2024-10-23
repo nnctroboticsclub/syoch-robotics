266677dcdf95e9b662e33fe374d35eafc53a3a6b
syoch <syoch64@gmail.com>
2024-10-23 08:16:58 +0000

    some

--- libs/network/rd16/include/rd16.hpp
+++ libs/network/rd16/include/rd16.hpp
@@ -3,21 +3,43 @@
 #include <cstdint>
 
 namespace robotics::network {
+// FNV-1A
+// TODO(syoch): Rename this class to FNV1A
 class RD16 {
-  uint16_t current = 0;
+  uint16_t current = 36683;
 
  public:
-  void Reset() { current = 0; }
+  void Reset() { current = 36683; }
 
-  void operator<<(uint16_t x) {
-    current = ((current & 0x007F) << 9) | ((current & 0xff80) >> 7);
-    current ^= 0x35ca;
+  void operator<<(uint8_t x) {
     current ^= x;
+    current *= 37003;
   }
 
-  void operator<<(uint8_t x) { *this << ((uint16_t)((x << 8) | x)); }
+  template <typename T>
+  void operator<<(std::vector<T> const& data) {
+    for (auto const& x : data) {
+      *this << x;
+    }
+  }
+
+  template <typename T>
+  static RD16 FromData(T const& data) {
+    RD16 rd16;
+    rd16 << data;
+    return rd16;
+  }
+
+  template <typename T>
+  RD16 CopyAndAppend(T const& data) const {
+    RD16 rd16 = *this;
+    rd16 << data;
+    return rd16;
+  }
 
   uint16_t Get() const { return current; }
+
+  void Set(uint16_t x) { current = x; }
 };
 
 }  // namespace robotics::network
