f07170651f58e95099aa1f91243a71f38a40bfd8
syoch <syoch64@gmail.com>
2025-01-03 05:59:56 +0000

    Update: Added span utility

---
+++ libs/common/include/robotics/utils/span.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <concepts>
+#include <cstddef>
+
+namespace robotics::utils {
+template <typename T>
+class Span {
+ public:
+  Span(T* data, size_t size) : data_(data), size_(size) {}
+  Span() : data_(nullptr), size_(0) {}
+
+  // copy
+  Span(Span<T>& other) = default;
+
+  template <std::convertible_to<T> U>
+  Span(const Span<U>& other) : data_(other.data()), size_(other.size()) {}
+
+  T* data() const { return data_; }
+  size_t size() const { return size_; }
+
+  T& operator[](size_t index) { return data_[index]; }
+
+ private:
+  T* data_;
+  size_t size_;
+};
+}  // namespace robotics::utils
