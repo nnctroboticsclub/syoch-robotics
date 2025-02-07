573f85d096463197c948674e29323dea31fc72ae
syoch <syoch64@gmail.com>
2025-02-07 16:42:29 +0000

    Update: Added LinkedList

--- libs/common/include/robotics/utils/linked_list_node.hpp
+++ libs/common/include/robotics/utils/linked_list_node.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 namespace robotics::utils {
 template <typename T>
 class LinkedListNode {
@@ -10,8 +11,105 @@ class LinkedListNode {
   auto& Next() { return *next; }
   auto Next(T& ptr) { next = &ptr; }
 
- private:
+  bool InUse() { return in_use; }
+  void InUse(bool value) { in_use = value; }
+
+  void ResetLinkNode() {
+    prev = nullptr;
+    next = nullptr;
+  }
+
+ public:
+  bool in_use = false;
   T* prev = 0;
   T* next = 0;
+};
+
+template <typename T, std::size_t N>
+class LinkedList {
+ public:
+  std::array<T, N> nodes;
+  T* head = nullptr;
+  T* tail = nullptr;
+
+  T* NewNode() {
+    for (auto& node : nodes) {
+      if (!node.InUse()) {
+        node.InUse(true);
+        return &node;
+      }
+    }
+
+    return nullptr;
+  }
+
+ public:
+  class Iterator {
+    T* current;
+
+   public:
+    Iterator(T* node) : current(node) {}
+
+    T* operator*() { return current; }
+
+    Iterator& operator++() {
+      current = &current->Next();
+      return *this;
+    }
+
+    bool operator!=(const Iterator& other) { return current != other.current; }
+  };
+
+ public:
+  LinkedList() {
+    for (auto& node : nodes) {
+      node.ResetLinkNode();
+    }
+  }
+
+  auto head_node() { return head; }
+  auto tail_node() { return tail; }
+
+  auto begin() { return Iterator(head); }
+  auto end() { return Iterator(nullptr); }
+
+  void Remove(T* node) {
+    node->InUse(false);
+
+    if (node == head) {
+      head = &node->Next();
+    }
+
+    if (node == tail) {
+      tail = &node->Prev();
+    }
+
+    if (&node->Prev() != nullptr) {
+      node->Prev().Next(node->Next());
+    }
+
+    if (&node->Next() != nullptr) {
+      node->Next().Prev(node->Prev());
+    }
+  }
+
+  T* NewBack() {
+    auto new_node = NewNode();
+    if (new_node == nullptr) {
+      return nullptr;
+    }
+
+    if (head == nullptr) {
+      head = new_node;
+      tail = new_node;
+      return new_node;
+    }
+
+    tail->Next(*new_node);
+    new_node->Prev(*tail);
+    tail = new_node;
+
+    return new_node;
+  }
 };
 }  // namespace robotics::utils
