810e7e53b2ab353648ffce84eefabe44f35e3e4f
syoch <syoch64@gmail.com>
2025-03-01 06:01:55 +0000

    Update(logger): Controlable ChainedLogSink's sink2 activity

--- libs/logger/include/logger/chained_log_sink.hpp
+++ libs/logger/include/logger/chained_log_sink.hpp
@@ -7,17 +7,24 @@ class ChainedLogger final : public robotics::logger::LogSink {
   robotics::logger::LogSink* sink1;
   robotics::logger::LogSink* sink2;
 
+  bool sink2_is_active = true;
+
  public:
   ChainedLogger(robotics::logger::LogSink* sink1,
                 robotics::logger::LogSink* sink2)
       : sink1(sink1), sink2(sink2) {}
 
+  [[nodiscard]] bool Sink2Active() const { return sink2_is_active; }
+  void Sink2Active(bool is_active) { sink2_is_active = is_active; }
+
   ~ChainedLogger() override = default;
 
-  virtual void Log(robotics::logger::core::Level level, const char* tag,
-                   const char* msg) final {
+  void Log(robotics::logger::core::Level level, const char* tag,
+           const char* msg) final {
     sink1->Log(level, tag, msg);
-    sink2->Log(level, tag, msg);
+    if (sink2_is_active) {
+      sink2->Log(level, tag, msg);
+    }
   }
 };
 }  // namespace robotics::logger
