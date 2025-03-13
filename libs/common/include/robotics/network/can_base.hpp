d5f09a87e278340d6d4b279b88535c7731a61974
syoch <syoch64@gmail.com>
2025-03-13 23:32:21 +0000

    Update: Added CANRef Type

--- libs/common/include/robotics/network/can_base.hpp
+++ libs/common/include/robotics/network/can_base.hpp
@@ -2,15 +2,16 @@
 
 #include <cstdint>
 #include <functional>
+#include <memory>
 #include <vector>
 
 namespace robotics::network {
 class CANBase {
  public:
   using RxCallback =
-      std::function<void(std::uint32_t, std::vector<uint8_t> const &)>;
+      std::function<void(std::uint32_t, std::vector<uint8_t> const&)>;
   using TxCallback =
-      std::function<void(std::uint32_t, std::vector<uint8_t> const &)>;
+      std::function<void(std::uint32_t, std::vector<uint8_t> const&)>;
   using IdleCallback = std::function<void()>;
 
   struct Capability {
@@ -19,7 +20,7 @@ class CANBase {
 
   virtual void Init() = 0;
 
-  virtual int Send(std::uint32_t id, std::vector<uint8_t> const &data) = 0;
+  virtual int Send(std::uint32_t id, std::vector<uint8_t> const& data) = 0;
 
   virtual void OnRx(RxCallback cb) = 0;
   virtual void OnTx(TxCallback cb) = 0;
@@ -28,4 +29,6 @@ class CANBase {
   virtual Capability GetCapability();
   virtual float GetBusLoad();
 };
+
+using CANRef = std::shared_ptr<CANBase>;
 }  // namespace robotics::network
