87a50c53b4e98d2329660550635a8a0b70ece303
syoch <syoch64@gmail.com>
2025-03-18 04:12:42 +0000

    Update(ContextDetector): Extended support range from mbed to cortex m4

--- libs/binary/context-detector/include/robotics/binary/context_detector.hpp
+++ libs/binary/context-detector/include/robotics/binary/context_detector.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#if SR_BCD_CORTEX_M4
+#include <cstdint>
+#endif
+
 namespace robotics::binary {
 
 inline bool InNonWaitableContext() {
-#if SR_BCD_PL_MBED
+#if SR_BCD_CORTEX_M4
   auto* icsr = reinterpret_cast<volatile uint32_t*>(0xE000ED04);
   return (*icsr & 0x000000ff) != 0;  // VECTACTIVE
 #else
