edc6c89c5b90bbf4000e34aecfa93ace20b946bb
syoch <syoch64@gmail.com>
2025-03-18 04:13:16 +0000

    Update(TempBuf): Splitted buffer type and reduced buffer size

--- libs/binary/temp-buffer/include/robotics/binary/temp_buffer.hpp
+++ libs/binary/temp-buffer/include/robotics/binary/temp_buffer.hpp
@@ -3,4 +3,7 @@
 namespace robotics::binary {
 /// @brief 同時に 2 箇所以上で使用できないバッファを取得する
 void* GetTemporaryBuffer();
+
+/// @brief アプリケーションサイドのバッファを取得する
+void* GetTemporaryAppBuffer();
 }  // namespace robotics::binary
--- libs/binary/temp-buffer/source/temp_buffer.cpp
+++ libs/binary/temp-buffer/source/temp_buffer.cpp
@@ -1,7 +1,13 @@
+#include <robotics/binary/context_detector.hpp>
 #include <robotics/binary/temp_buffer.hpp>
 
-static char buf[0x100];
+static char buf_b[0x80];
+static char buf_app[0x80];
 
 void* robotics::binary::GetTemporaryBuffer() {
-  return buf;
+  return buf_b;
+}
+
+void* robotics::binary::GetTemporaryAppBuffer() {
+  return buf_app;
 }
