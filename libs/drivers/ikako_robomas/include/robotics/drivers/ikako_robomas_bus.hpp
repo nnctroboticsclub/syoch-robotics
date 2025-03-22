a517e7583cced09a79146febb0093987484e597f
syoch <syoch64@gmail.com>
2025-03-22 03:12:46 +0000

    Update: Commoized robomas implementation

--- libs/drivers/ikako_robomas/include/robotics/drivers/ikako_robomas_bus.hpp
+++ libs/drivers/ikako_robomas/include/robotics/drivers/ikako_robomas_bus.hpp
@@ -3,22 +3,33 @@
 #include <ikarashiCAN_mk2.h>
 #include <robotics/drivers/ikakorobomas_node.hpp>
 
-namespace nhk2024b::common {
+namespace robotics::drivers::ikako_robomas_bus {
+using robotics::drivers::ikako_robomas_node::IkakoRobomasNode;
+using robotics::drivers::ikako_robomas_node::RobomasWrapper;
+
 class IkakoRobomasBus {
  public:
-  IkakoRobomasBus(ikarashiCAN_mk2 &can) : sender(&can) {}
+  IkakoRobomasBus(ikarashiCAN_mk2& can) : sender(&can) {}
 
   int Write() { return sender.write(); }
 
   void Read() { sender.read(); }
 
-  template <
-      typename Motor,
-      std::enable_if_t<std::is_base_of<IkakoMotor, Motor>::value, bool> = true>
-  IkakoRobomasNode *NewNode(int index, Motor *motor) {
-    auto node = new IkakoRobomasNode(index, motor);
+  template <typename Motor>
+  RobomasWrapper* NewWrapper(
+      Motor* motor) requires std::is_base_of<IkakoMotor, Motor>::value {
+    auto node = new RobomasWrapper(motor);
     sender.set_motors(node->GetMotor()->get_motor());
     nodes.push_back(node);
+    return node;
+  }
+
+  template <typename Motor>
+  IkakoRobomasNode* NewNode(
+      Motor* motor) requires std::is_base_of<IkakoMotor, Motor>::value {
+    auto node = new IkakoRobomasNode(motor);
+    sender.set_motors(node->GetMotor()->get_motor());
+    nodes.push_back((RobomasWrapper*)node);
     return node;
   }
 
@@ -31,7 +42,11 @@ class IkakoRobomasBus {
 
   void Tick() { sender.read(); }
 
-  std::vector<IkakoRobomasNode *> nodes;
+  std::vector<RobomasWrapper*> nodes;
   IkakoRobomasSender sender;
 };
-}  // namespace nhk2024b::common
+}  // namespace robotics::drivers::ikako_robomas_bus
+
+namespace robotics::driver {
+using drivers::ikako_robomas_bus::IkakoRobomasBus;
+}
--- libs/drivers/ikako_robomas/include/robotics/drivers/ikakorobomas_node.hpp
+++ libs/drivers/ikako_robomas/include/robotics/drivers/ikakorobomas_node.hpp
@@ -1,11 +1,14 @@
+#pragma once
+
 #include <MotorController.h>
 #include <ikako_m3508.h>
 #include <ikarashiCAN_mk2.h>
 #include <mbed.h>
 
+#include <optional>
 #include <robotics/node/node.hpp>
-#include <vector>
 
+namespace robotics::drivers::ikako_robomas_node {
 struct controller_param {
   float Ts = 0.001;
   float kp = 1.0;
@@ -15,27 +18,22 @@ struct controller_param {
   float omega = 1 * 2 * M_PI;
 };
 
-class IkakoRobomasNode {
+class RobomasWrapper {
   controller_param cprm;
-  MotorParams *motor_params;
+  MotorParams* motor_params;
+  std::optional<ControlType> mode_ = std::nullopt;
+
+  MotorController* controller;
+  IkakoMotor* super;
+
+  friend class IkakoRobomasNode;
 
  public:
-  robotics::Node<float> velocity;
-  robotics::Node<float> factor;
-  MotorController *controller;
-  IkakoMotor *super;
-
- public:
-  template <
-      typename Motor,
-      std::enable_if_t<std::is_base_of<IkakoMotor, Motor>::value, bool> = true>
-  IkakoRobomasNode(
-      int index, Motor *motor, float current_limit = 20.0f
+  template <typename Motor>
+  RobomasWrapper(
+      Motor* motor, float current_limit = 20.0f
       // https://github.com/nnctroboticsclub/IkakoRobomasのm3という配列を指すポインタである
-  ) {
-    factor.SetValue(0.0f);
-    velocity.SetValue(0.0f);
-
+      ) requires std::is_base_of<IkakoMotor, Motor>::value {
     super = motor;
 
     motor_params = super->get_motor_params();
@@ -43,13 +41,60 @@ class IkakoRobomasNode {
     motor_params->J = 0.04;
 
     cprm.current_limit = current_limit;
+  }
 
-    controller = new MotorController(ControlType::VELOCITY, motor_params,
-                                     cprm.Ts, cprm.omega);
+  void SetControlType(ControlType type) {
+    if (controller != nullptr) {
+      delete controller;
+    }
+
+    controller = new MotorController(type, motor_params, cprm.Ts, cprm.omega);
     controller->set_limit(-cprm.current_limit, cprm.current_limit);
     controller->set_pid_gain(cprm.kp, cprm.ki, cprm.kd);
     controller->start();
 
+    mode_ = type;
+  }
+
+  void SetReference(float value) { controller->set_reference(value); }
+
+  IkakoMotor* GetMotor() { return super; }
+
+  bool GetReadFlag() { return super->get_read_flag(); }
+
+  // dt = 0.001 (1ms)
+  void Update() {
+    if (mode_ == std::nullopt) {
+      super->set_ref(0);
+      return;
+    }
+
+    auto mode = *mode_;
+
+    if (super->get_read_flag()) {
+      switch (mode) {
+        case ControlType::VELOCITY: {
+          controller->set_reference(super->get_vel());
+          break;
+        }
+        case ControlType::POSITION: {
+          controller->set_reference(super->get_angle());
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    }
+
+    controller->update();
+    super->set_ref(controller->get_output());
+  }
+};
+
+class IkakoRobomasNode : public RobomasWrapper {
+ public:
+  IkakoRobomasNode(IkakoMotor* motor) : RobomasWrapper(motor) {
     velocity.SetChangeCallback(
         [this](float velo) {  // veloとかは引数の名前で勝手に定義してよい
           // 処理をかけ
@@ -57,15 +102,14 @@ class IkakoRobomasNode {
         });
   }
 
-  IkakoMotor *GetMotor() { return super; }
-
-  bool GetReadFlag() { return super->get_read_flag(); }
-
-  // dt = 0.001 (1ms)
-  void Update() {
-    if (super->get_read_flag()) controller->set_response(super->get_vel());
-    controller->update();
-
-    super->set_ref(controller->get_output());
-  }
+ public:
+  robotics::Node<float> velocity;
+  robotics::Node<float> factor;
 };
+
+}  // namespace robotics::drivers::ikako_robomas_node
+
+namespace robotics::driver {
+using drivers::ikako_robomas_node::IkakoRobomasNode;
+using drivers::ikako_robomas_node::RobomasWrapper;
+}  // namespace robotics::driver
