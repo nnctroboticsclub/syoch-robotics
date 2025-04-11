cd4229ec242563d64ee887c4c70629aa203846ac
syoch <syoch64@gmail.com>
2025-04-11 14:42:19 +0000

    Update: Reverted logger system

--- libs/logger/source/logger-core.cpp
+++ libs/logger/source/logger-core.cpp
@@ -4,14 +4,93 @@
 #include <cstdio>
 #include <cstring>
 
+#ifdef USE_THREAD
+#include <robotics/thread/thread.hpp>
+#endif
+
+#include <robotics/binary/context_detector.hpp>
 #include <robotics/binary/temp_buffer.hpp>
+#include <robotics/utils/no_mutex_lifo.hpp>
+#include <robotics/utils/span.hpp>
 
 #include <logger/log_sink.hpp>
 
+using robotics::utils::Span;
+
+namespace {
+#if defined(LOG_FOR_MBED)
+#if TARGET_NUCLEO_F303K8
+const size_t kLogRingBufferSize = 0x100;
+const size_t kLogLineSize = 0x80;
+const size_t kMaxLogLines = 3;
+#elif TARGET_NUCLEO_F446RE
+const size_t kLogRingBufferSize = 0x8000;
+const size_t kLogLineSize = 0x100;
+const size_t kMaxLogLines = 200;
+#else
+const size_t kLogRingBufferSize = 0x100;
+const size_t kLogLineSize = 0x80;
+const size_t kMaxLogLines = 10;
+#endif  // ^ NUCLEO_F303K8
+#else   // ^ LOG_FOR_MBED
+const size_t kLogRingBufferSize = 0x8000;
+const size_t kLogLineSize = 0x100;
+const size_t kMaxLogLines = 200;
+#endif
+
+char log_ring_buffer[kLogRingBufferSize] = {};
+size_t log_head = 0;
+
+void UseLine(size_t start, size_t length, char* dest) {
+  size_t offset;
+
+  for (offset = 0; offset < length; offset++) {
+    size_t index = (start + offset) % kLogRingBufferSize;
+
+    dest[offset] = log_ring_buffer[index];
+  }
+
+  dest[offset] = 0;  // null-terminate for c-style strings
+}
+
+Span<char> PasteToRing(Span<const char> text) {
+  size_t start = log_head;
+  for (size_t offset = 0; offset < text.size(); offset++) {
+    size_t index = (start + offset) % kLogRingBufferSize;
+
+    log_ring_buffer[index] = text[offset];
+  }
+
+  log_head = (start + text.size()) % kLogRingBufferSize;
+
+  return {log_ring_buffer + start, text.size()};
+}
+}  // namespace
+
 namespace robotics::logger::core {
 
-void inline Log(Level level, const char* tag, const char* msg, size_t) {
-  global_log_sink->Log(level, tag, msg);
+struct LogLine {
+  Level level;
+  Span<const char> tag;
+  Span<const char> msg;
+
+  LogLine() = default;
+
+  LogLine(Span<const char> tag, Span<const char> msg,
+          Level level = Level::kInfo)
+      : level(level),           //
+        tag(PasteToRing(tag)),  //
+        msg(PasteToRing(msg)) {}
+};
+
+static robotics::utils::NoMutexLIFO<LogLine, kMaxLogLines> log_queue;
+
+void inline Log(Level level, const char* tag, const char* msg, size_t msg_len) {
+  if (robotics::binary::InNonWaitableContext()) {
+    log_queue.Push(LogLine({tag, strlen(tag)}, {msg, msg_len}, level));
+  } else {
+    global_log_sink->Log(level, tag, msg);
+  }
 }
 
 void Log(Level level, const char* tag, const char* msg) {
@@ -31,8 +110,55 @@ void LogHex(Level level, const char* tag, uint8_t* data, uint32_t length) {
   Log(level, tag, buffer, 2 * length);
 }
 
-void LoggerProcess() {}
+void LoggerProcess() {
+  std::array<char, 64> tag_buf;
+  std::array<char, kLogLineSize> msg_buf;
 
-void Init() {}
+  while (!log_queue.Empty()) {
+    auto line = log_queue.Pop();
+
+    UseLine(line.tag.data() - log_ring_buffer, line.tag.size(), tag_buf.data());
+    UseLine(line.msg.data() - log_ring_buffer, line.msg.size(), msg_buf.data());
+
+    global_log_sink->Log(line.level, tag_buf.data(), msg_buf.data());
+  }
+}
+
+void Init() {
+  LogLine dummy_log_line({}, {});
+
+  while (!log_queue.Full()) {
+    log_queue.Push(dummy_log_line);
+  }
+
+  while (!log_queue.Empty()) {
+    log_queue.Pop();
+  }
+
+#ifdef USE_THREAD
+  StartLoggerThread();
+#endif
+}
+
+#ifdef USE_THREAD
+
+robotics::system::Thread* logger_thread;
+
+[[noreturn]] static void Thread() {
+  while (true) {
+    LoggerProcess();
+  }
+}
+
+void StartLoggerThread() {
+
+  if (logger_thread)
+    return;
+  logger_thread = new robotics::system::Thread();
+  logger_thread->SetThreadName("Logger");
+  logger_thread->Start(Thread);
+}
+
+#endif
 
 }  // namespace robotics::logger::core
